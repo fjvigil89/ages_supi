0a84911cb3cf69fe8f1d923337eead43de3e0f89
curonny <rmontano1992@gmail.com>
2022-04-06 11:12:04 -0400

    fix bug in register user

--- supi_integration/controllers/controllers.py
+++ supi_integration/controllers/controllers.py
@@ -54,11 +54,11 @@ class AuthRegisterHome(Home):
 
         qcontext = self.get_auth_signup_qcontext()
 
-        if qcontext.get('data'):
+        if kw.get('data'):
             if 'error' not in qcontext and request.httprequest.method == 'POST':
                 try:
 
-                    user = request.env['res.users'].sudo().create(qcontext.get('data'))
+                    user = request.env['res.users'].sudo().create(kw.get('data'))
 
                     return {
                         "jsonrpc": "2.0",
@@ -76,34 +76,3 @@ class AuthRegisterHome(Home):
                     else:
                         _logger.error("%s", e)
                         qcontext['error'] = _("No se puede crear la cuenta")
-        else:
-
-            if not qcontext.get('token') and not qcontext.get('signup_enabled'):
-                raise werkzeug.exceptions.NotFound()
-
-            if 'error' not in qcontext and request.httprequest.method == 'POST':
-                try:
-                    self.do_signup(qcontext)
-                    # Send an account creation confirmation email
-                    if qcontext.get('token'):
-                        User = request.env['res.users']
-                        user_sudo = User.sudo().search(
-                            User._get_login_domain(qcontext.get('login')), order=User._get_login_order(), limit=1
-                        )
-                        template = request.env.ref('auth_signup.mail_template_user_signup_account_created',
-                                                   raise_if_not_found=False)
-                        if user_sudo and template:
-                            template.sudo().send_mail(user_sudo.id, force_send=True)
-                    return self.web_login(*args, **kw)
-                except UserError as e:
-                    qcontext['error'] = e.args[0]
-                except (SignupError, AssertionError) as e:
-                    if request.env["res.users"].sudo().search([("login", "=", qcontext.get("login"))]):
-                        qcontext["error"] = _("Another user is already registered using this email address.")
-                    else:
-                        _logger.error("%s", e)
-                        qcontext['error'] = _("Could not create a new account.")
-
-            response = request.render('auth_signup.signup', qcontext)
-            response.headers['X-Frame-Options'] = 'DENY'
-            return response
