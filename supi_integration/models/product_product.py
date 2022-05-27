3cdadb03e805bfc0c207dae5cf8cb1e9203a722f
curonny <rmontano1992@gmail.com>
2022-05-26 23:09:21 -0400

    products config

--- supi_integration/models/product_product.py
+++ supi_integration/models/product_product.py
@@ -7,3 +7,10 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     pack = fields.Integer(string="Paquete")
+    partner_id = fields.Many2one("res.partner", string="Cliente")
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    can_be_mueble = fields.Boolean(string="Puede ser mueble")
